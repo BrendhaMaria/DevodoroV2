1969821de4df707fe2b7988ae6e284068db19a6a
brendha <brendh.martins@gmail.com>
2026-04-24 10:13:14 -0300

    melhorias no css(modo claro e escudo, configurações)

---
+++ js/modoescuro.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("escuro");
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// carregar tema salvo
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
